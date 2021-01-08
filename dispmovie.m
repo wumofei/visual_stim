cb61a2b5660ee01e53854b3d227bf0fae3f766ef
wumofei <mwumofei@gmail.com>
2021-01-08 11:45:47 -0600

    Ensure better timing
    Instead of PlayMovie, load video into matrix. Make each frame into a
    texture, then draw textures.

--- dispmovie.m
+++ dispmovie.m
@@ -1,18 +1,45 @@
-function dispmovie(movie_filename, varargin)
+function dispmovie(varargin)
 
 %% Check inputs.
+if nargin == 0
+    error('At least one input required: video filename or variable');
+end
+
 p = inputParser;
 v = @validateattributes;
 
-addRequired(p,'movie_filename',   @(x) v(x,{'char'},{'nonempty'},mfilename,'movie_filename',1));
-addOptional(p,'screenNumber', [], @(x) v(x,{'numeric'},{'scalar','integer','nonnegative'}));
+addParameter(p, 'video_filename', [],  @(x) v(x,{'char'},{'nonempty'},mfilename,'video_filename'));
+addParameter(p, 'videomat', [], @(x) v(x,{'numeric'},{'nonempty','nonnan','nonnegative'},mfilename,'videomat'));
+addParameter(p, 'video_fps', [], @(x) v(x,{'numeric'},{'scalar','nonnegative'},mfilename,'video_fps'));
+addParameter(p, 'screenNumber', [], @(x) v(x,{'numeric'},{'scalar','integer','nonnegative'},mfilename,'screenNumber'));
 
-parse(p, movie_filename, varargin{:});
+parse(p, varargin{:});
 
+video_filename = p.Results.video_filename;
+videomat = p.Results.videomat;
+video_fps = p.Results.video_fps;
 screenNumber = p.Results.screenNumber;
-clearvars p v
+clearvars varargin p v
 
-%% Configure Psychtoolbox.
+%% Import movie.
+if ~bitxor(isempty(video_filename), isempty(videomat))
+    error('Precisely one video filename or video variable is required.')
+end
+
+if isempty(videomat)
+    videoobj = VideoReader(video_filename);
+    videomat = squeeze(read(videoobj));
+    video_fps = videoobj.Framerate;
+    clearvars video_filename
+elseif isempty(video_fps)
+    warning('Input video FPS not given. Assuming 60 FPS.')
+    video_fps = 60;
+end
+
+videodim = size(videomat);
+video_ifi = 1 / video_fps;
+
+%% Initiate Psychtoolbox.
 PsychDefaultSetup(2);
 KbReleaseWait;
 
@@ -22,31 +49,51 @@ end
 
 black = BlackIndex(screenNumber);
 
-%% Display movie.
 try
+    % Configure Psychtoolbox.
     Screen('Preference', 'SkipSyncTests', 0);
     oldVerbosityLevel = Screen('Preference', 'Verbosity', 0);
     oldVisualDebugLevel = Screen('Preference', 'VisualDebugLevel', 1);
-    window = PsychImaging('OpenWindow', screenNumber, black);
-    movie = Screen('OpenMovie', window, movie_filename);
-    topPriorityLevel = MaxPriority(window);
-    HideCursor(window);
-    Priority(topPriorityLevel);
-    Screen('PlayMovie', movie, 1);
-
-    while ~KbCheck
-        tex = Screen('GetMovieImage', window, movie);
-        if tex<=0
-            break;
-        end
     
-        Screen('DrawTexture', window, tex);
-        Screen('Flip', window);
-        Screen('Close', tex);
+    %% Open and configure window.
+    [window, windowRect] = PsychImaging('OpenWindow', screenNumber, black);
+    [screenXpx, screenYpx] = Screen('WindowSize', window);
+    screen_ifi = Screen('GetFlipInterval',window);
+    topPriorityLevel = MaxPriority(window);
+    Screen('BlendFunction', window, 'GL_SRC_ALPHA', 'GL_ONE_MINUS_SRC_ALPHA');
+    
+    if screen_ifi > video_ifi
+        warning('Screen FPS is lower than input movie FPS by %.2f%%. May result in inaccurate frame timing.', (video_fps - 1/screen_ifi) / video_fps * 100)
     end
+    
+    if videodim(1) ~= screenYpx || videodim(2) ~= screenXpx
+        warning('Input movie (%i,%i) and output screen (%i,%i) do not have the same dimensions.', videodim(2), videodim(1), screenXpx, screenYpx);
+    end
+    
+    %% Convert movie frames to textures.
+    frametex = NaN(videodim(end),1);
+    for i = 1:videodim(end)
+        frametex(i) = Screen('MakeTexture', window, videomat(:,:,i));
+    end
+    clearvars videomat
+    
+    %% Show movie.
+    % Configure window to prepare to show movie.
+    HideCursor(window);
+    KbReleaseWait;
+    Priority(topPriorityLevel);
+    vbl = Screen('Flip', window);
+    
+    % Display movie.
+    tic;
+    for i = 1:videodim(end)
+        Screen('DrawTexture', window, frametex(i));
+        vbl = Screen('Flip', window, vbl + 0.75*video_ifi);
+        Screen('Close', frametex(i));
+    end
+    toc
 
-    Screen('PlayMovie', movie, 0);
-    Screen('CloseMovie', movie);
+    %% Restore original Psychtoolbox settings and close screen.
     Screen('Preference', 'Verbosity', oldVerbosityLevel);
     Screen('Preference', 'VisualDebugLevel', oldVisualDebugLevel);
     Priority(0);
