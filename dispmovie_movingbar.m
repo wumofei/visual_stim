f754355b8bedbfc00deec23644b464572349e603
wumofei <mwumofei@gmail.com>
2021-01-11 15:56:37 -0600

    Initiate moving bar

--- dispmovie_movingbar.m
+++ dispmovie_movingbar.m
@@ -12,6 +12,13 @@ addParameter(p, 'video_filename', [],  @(x) v(x,{'char'},{'nonempty'},mfilename,
 addParameter(p, 'videomat', [], @(x) v(x,{'numeric'},{'nonempty','nonnan','nonnegative'},mfilename,'videomat'));
 addParameter(p, 'video_fps', [], @(x) v(x,{'numeric'},{'scalar','nonnegative'},mfilename,'video_fps'));
 addParameter(p, 'screenNumber', [], @(x) v(x,{'numeric'},{'scalar','integer','nonnegative'},mfilename,'screenNumber'));
+addParameter(p, 'movingbar', 0, @(x) v(x,{'numeric'},{'scalar','binary'},mfilename,'movingbar'));
+addParameter(p, 'nReps', 1, @(x) v(x,{'numeric'},{'scalar','positive','integer'},mfilename,'nReps'));
+addParameter(p, 'nDirections', 4, @(x) v(x,{'numeric'},{'scalar','positive','integer'},mfilename,'nDirections'));
+addParameter(p, 'bar_width', 100, @(x) v(x,{'numeric'},{'scalar','positive'},mfilename,'bar_width'));
+addParameter(p, 'bar_length', 800, @(x) v(x,{'numeric'},{'scalar','positive'},mfilename,'bar_length'));
+addParameter(p, 'bar_speed', 300, @(x) v(x,{'numeric'},{'scalar','positive'},mfilename,'bar_speed'));
+addParameter(p, 'bar_color', 1, @(x) v(x,{'numeric'},{'2d','>=',0,'<=',1},mfilename,'bar_color'));
 
 parse(p, varargin{:});
 
@@ -19,6 +26,14 @@ video_filename = p.Results.video_filename;
 videomat = p.Results.videomat;
 video_fps = p.Results.video_fps;
 screenNumber = p.Results.screenNumber;
+movingbar = p.Results.movingbar;
+nReps = p.Results.nReps;
+nDirections = p.Results.nDirections;
+bar_width = p.Results.bar_width;
+bar_length = p.Results.bar_length;
+bar_speed = p.Results.bar_speed;
+bar_color = p.Results.bar_color;
+
 clearvars varargin p v
 
 %% Import movie.
@@ -38,6 +53,19 @@ end
 
 videodim = size(videomat);
 video_ifi = 1 / video_fps;
+
+%% Optionally, create moving bar matrix.
+if movingbar
+    directions = repmat(0:(360/nDirections):(360-(360/nDirections)),nReps,1);
+    for iRep = 1:nReps
+        directions(iRep,:) = directions(iRep,randperm(nDirections));
+    end
+    disp(directions);
+    
+    barmat = ones(bar_width,bar_length+2,2);
+    barmat(:,:,1) = barmat(:,:,1) * bar_color;
+    barmat(:,2:end-1,2) = 0;
+end
 
 %% Initiate Psychtoolbox.
 PsychDefaultSetup(2);
@@ -63,7 +91,7 @@ try
     Screen('BlendFunction', window, 'GL_SRC_ALPHA', 'GL_ONE_MINUS_SRC_ALPHA');
     
     if screen_ifi > video_ifi
-        warning('Screen FPS is lower than input movie FPS by %.2f%%. May result in inaccurate frame timing.', (video_fps - 1/screen_ifi) / video_fps * 100)
+        warning('Input video FPS exceeds screen FPS by %.2f%%. May result in inaccurate frame timing.', (video_fps - 1/screen_ifi) / video_fps * 100)
     end
     
     if videodim(1) ~= screenYpx || videodim(2) ~= screenXpx
@@ -76,6 +104,10 @@ try
         frametex(i) = Screen('MakeTexture', window, videomat(:,:,i));
     end
     clearvars videomat
+    
+    if movingbar
+        bartex = Screen('MakeTexture', window, barmat);
+    end
     
     %% Show movie.
     % Configure window to prepare to show movie.
