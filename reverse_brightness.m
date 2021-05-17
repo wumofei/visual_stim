b36f1674c46d7ac8c5021e1200a3f167811038c3
wumofei <mwumofei@gmail.com>
2021-05-17 14:37:25 -0500

    Allow strings for input video argument

--- reverse_brightness.m
+++ reverse_brightness.m
@@ -8,7 +8,7 @@ function outmat = reverse_brightness(input_video, varargin)
 p = inputParser;
 v = @validateattributes;
 
-addRequired( p, 'input_video',     @(x) v(x,{'char','numeric'},{'nonempty'},mfilename,'input_video'));    % filename or matrix.
+addRequired( p, 'input_video',     @(x) v(x,{'char','string','numeric'},{'nonempty'},mfilename,'input_video'));    % filename or matrix.
 addParameter(p, 'video_fps',   [], @(x) v(x,{'numeric'},{'scalar','nonnegative'},mfilename,'video_fps')); % default 60 fps.
 
 parse(p, input_video, varargin{:});
