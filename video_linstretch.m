1c0f64dc726cac26f046b2fe3f110b41d8a40ff3
wumofei <mwumofei@gmail.com>
2021-05-17 12:36:20 -0500

    Process video frame-by-frame

--- video_linstretch.m
+++ video_linstretch.m
@@ -1,39 +1,73 @@
-function output_video = video_linstretch(input_filename, dMean, dContrast)
+function output_video = video_linstretch(input_video, dMean, dContrast)
 
-%% Read input video.
-input_videoobj = VideoReader(input_filename);
-[~,filename,~] = fileparts(input_filename);
-input_fps = input_videoobj.Framerate;
-clearvars input_filename
+% Check inputs.
+if nargin < 3
+    error('3 inputs required: input video, desired change in mean intensity, and desired change in contrast.')
+elseif ~ischar(input_video) && ~ismatrix(input_video)
+    error('Input video must be given as filename or matrix.')
+elseif ~isscalar(dMean) || dMean <= 0
+    error('Desired change in mean intensity must be given as a positive scalar.')
+elseif ~isscalar(dContrast) || dContrast <= 0
+    error('Desired change in contrast must be given as a positive scalar.')
+end
 
-% Extract frames from video object.
-input_video = im2double(squeeze(read(input_videoobj, [1 input_videoobj.Duration*input_fps])));
-clearvars input_videoobj
-
-%% Find original mean and contrast.
-oldMin = min(input_video(:));
-oldMax = max(input_video(:));
-oldContrast = (oldMax - oldMin) / (oldMax + oldMin);
-
-oldMean = mean2(input_video);
-oldMean_norm = (oldMean - oldMin) / (oldMax - oldMin);
-
-%% Determine new range to match desired change in mean and contrast.
-newMean = oldMean * dMean;
-newContrast = oldContrast * dContrast;
-
-newMin = newMean / oldMean_norm / ((1 + newContrast) / (1 - newContrast) + 1/(oldMean_norm) - 1);
-newMax = newMin * (1 + newContrast) / (1 - newContrast);
-
-%% Linearly stretch video to desired range.
-output_video = (input_video - oldMin) / (oldMax - oldMin) * (newMax - newMin) + newMin;
-output_video = max(0, min(1, output_video));
-
-%% Write output video.
-output_file = VideoWriter([filename '_linstretch.avi'], 'Grayscale AVI');
-output_file.FrameRate = input_fps;
-open(output_file);
-writeVideo(output_file, output_video);
-close(output_file);
-
+if ischar(input_video)
+    % Read input video.
+    videoObj = VideoReader(input_video);
+    % Record min, max, and mean for each frame.
+    frameMin = NaN(1,videoObj.NumFrames);
+    frameMax = NaN(1,videoObj.NumFrames);
+    frameMean = NaN(1,videoObj.NumFrames);
+    for i = 1:videoObj.NumFrames
+        frame = squeeze(read(videoObj,i));
+        frameMin(i) = min(frame(:));
+        frameMax(i) = max(frame(:));
+        frameMean(i) = mean2(frame);
+    end
+    % Find min, max, and mean for entire video.
+    oldMin = min(frameMin);
+    oldMax = max(frameMax);
+    oldMean = mean(frameMean);
+    if oldMax == oldMin
+        error('Input video is uniform.')
+    end
+    oldContrast = (oldMax - oldMin) / (oldMax + oldMin);
+    oldMean_norm = (oldMean - oldMin) / (oldMax - oldMin);
+    newMean = oldMean * dMean;
+    newContrast = oldContrast * dContrast;
+    % Find new range to match desired change in mean intensity and contrast.
+    newMin = newMean / oldMean_norm / ((1 + newContrast) / (1 - newContrast) + 1/(oldMean_norm) - 1);
+    newMax = newMin * (1 + newContrast) / (1 - newContrast);
+    % Linearly stretch frames to desired range.
+    [~,filename] = fileparts(videoObj.Name);
+    mkdir(videoObj.Path, [filename sprintf('_dMean%i_dContrast%i', dMean*100, dContrast*100)]);
+    for i = 1:videoObj.NumFrames
+        frame = squeeze(read(videoObj,i));
+        frame = (frame - oldMin) / (oldMax - oldMin) * (newMax - newMin) + newMin;
+        imwrite(frame, fullfile(videoObj.Path, [filename sprintf('_dMean%i_dContrast%i', dMean*100, dContrast*100)], sprintf('frame%i.tif',i)));
+    end
+    % Combine frames into output video.
+    outfile = VideoWriter(fullfile(videoObj.Path, [filename sprintf('_dMean%i_dContrast%i.avi', dMean*100, dContrast*100)]), 'Grayscale AVI');
+    outfile.FrameRate = videoObj.FrameRate;
+    open(outfile);
+    for i = 1:videoObj.NumFrames
+        frame = imread(fullfile(videoObj.Path, [filename sprintf('_dMean%i_dContrast%i', dMean*100, dContrast*100)], sprintf('frame%i.tif',i)));
+        writeVideo(outfile,frame)
+    end
+    close(outfile);
+else
+    % Find original mean and contrast.
+    oldMin = min(input_video(:));
+    oldMax = max(input_video(:));
+    oldContrast = (oldMax - oldMin) / (oldMax + oldMin);
+    oldMean = mean2(input_video);
+    oldMean_norm = (oldMean - oldMin) / (oldMax - oldMin);
+    % Determine new range to match desired change in mean and contrast.
+    newMean = oldMean * dMean;
+    newContrast = oldContrast * dContrast;
+    newMin = newMean / oldMean_norm / ((1 + newContrast) / (1 - newContrast) + 1/(oldMean_norm) - 1);
+    newMax = newMin * (1 + newContrast) / (1 - newContrast);
+    % Linearly stretch video to desired range.
+    output_video = (input_video - oldMin) / (oldMax - oldMin) * (newMax - newMin) + newMin;
+    output_video = max(0, min(1, output_video));
 end
